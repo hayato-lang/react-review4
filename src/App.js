bf751486f30e2bfb9a26bc0d376b1a33855bf376
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-11-12 08:15:17 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  const onClickCountUp = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div className="App">
+      <p>{count}</p>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+    </div>
+  );
+}
